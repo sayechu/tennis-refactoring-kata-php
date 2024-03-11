06db2c24b319323284351fdd97f90e242e9a21b5
Santi Ayechu <santiayechu@gmail.com>
2024-03-11 18:23:38 +0100

    add methods to clean code -> isAdvantage() + isThereAWinner() + isDeuce() + isAdvantageDeuceOrWinner()

--- src/TennisGame1.php
+++ src/TennisGame1.php
@@ -18,7 +18,8 @@ class TennisGame1 implements TennisGame
 
     public function wonPoint($playerName): void
     {
-        if ($this->player1Name == $playerName) {
+        if ($this->player1Name == $playerName)
+        {
             $this->scorePlayer1++;
         } else {
             $this->scorePlayer2++;
@@ -28,29 +29,71 @@ class TennisGame1 implements TennisGame
     public function getScore(): string
     {
         $score = "";
-        if (($this->scorePlayer1 >= 4 || $this->scorePlayer2 >= 4) || ($this->scorePlayer1 == 3 && $this->scorePlayer2 == 3)) {
+        if ($this->isAdvantageDeuceOrWinner($this->scorePlayer1, $this->scorePlayer2))
+        {
             return $this->getAdvantageOrWinScore($this->scorePlayer1, $this->scorePlayer2);
         }
 
         $score = self::PLAYER_SCORES[$this->scorePlayer1];
-        if ($this->scorePlayer1 == $this->scorePlayer2) {
+        if ($this->scorePlayer1 == $this->scorePlayer2)
+        {
             return $score . "-All";
         }
         return ($score . "-" . self::PLAYER_SCORES[$this->scorePlayer2]);
     }
 
-    private function getAdvantageOrWinScore($scorePlayer1, $scorePlayer2): string {
+    private function getAdvantageOrWinScore($scorePlayer1, $scorePlayer2): string
+    {
         $scoreDifference = $scorePlayer1 - $scorePlayer2;
-        if ($scoreDifference == 0) {
+        if ($this->isDeuce($scoreDifference))
+        {
             return "Deuce";
         }
-        if ($scoreDifference == 1 || $scoreDifference == -1) {
+        if ($this->isAdvantage(($scoreDifference)))
+        {
             return "Advantage" . " " . ($scoreDifference == 1 ? $this->player1Name : $this->player2Name);
         }
-        if ($scoreDifference == 2) {
+        if ($this->isThereAWinner($scoreDifference))
+        {
             return "Win for" . " " . $this->player1Name;
         }
         return "Win for" . " " . $this->player2Name;
+    }
+
+    private function isAdvantage($scoreDifference): bool
+    {
+        if (abs($scoreDifference) == 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private function isThereAWinner($scoreDifference): bool
+    {
+        if ($scoreDifference >= 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private function isDeuce($scoreDifference): bool
+    {
+        if ($scoreDifference == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private function isAdvantageDeuceOrWinner(int $scorePlayer1, int $scorePlayer2): bool
+    {
+        if (($scorePlayer1 >= 4 || $scorePlayer2 >= 4) || ($scorePlayer1 == 3 && $scorePlayer2 == 3))
+        {
+            return true;
+        }
+        return false;
     }
 }
 
